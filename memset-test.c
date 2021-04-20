9c811e339a759f5f2b295f30e5fc9b394ef03591
Piri Veeta <pveeta@am-l3.msk.21-school.ru>
2021-04-20 17:20:08 +0300

    memset

---
+++ memset-test.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+
+int main (void)
+{
+	size_t len = 4;
+	int i = '5';
+	char str[10] =  "lalal";
+ 	ft_memset(str, i, len);
+	return (0);
+}
